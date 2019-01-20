d44318e6eba1a63881dcc2a0803a2cbadfb45bde
NotMobisMan <iamnotmobisman@gmail.com>
2019-01-20 14:51:48 +0900

    Implement up & down api

--- main.py
+++ main.py
@@ -11,22 +11,70 @@ app.config['SECRET_KEY'] = '123456790'
 admin = flask_admin.Admin(app, name="Admin")
 
 api = Api(app,doc='/apidoc/',version='1.0')
+"""
 
+"""
 @api.route('/hello')
 class HelloWorld(Resource):
     def get(self):
-        return {'hello' : 'world'}
+        return {'hello' : Restroom.get_list().fetch()}
 
 @app.route('/main')
 def index_page():
-    return render_template('index.html')
+    restrooms = Restroom.get_list().fetch();
+    return render_template('index.html',restrooms = restrooms)
 
+# API for increasing the number of users
+# It means that the number of vacant rooms will be decreased.
+@api.route('/Up/<string:url>')
+class Up(Resource):
+    def get(self,url):
+        targetKey = ndb.Key(urlsafe=url)
+        target = targetKey.get()
+        
+        NumOfVacantRooms = target.NumOfVacantRooms
+        Before = NumOfVacantRooms
+
+        if NumOfVacantRooms > 0 :
+            NumOfVacantRooms = NumOfVacantRooms - 1
+        target.NumOfVacantRooms = NumOfVacantRooms
+        target.put()
+        After = NumOfVacantRooms
+
+        return {'status' : 'ok', 'before':Before, 'after' : After}
+
+
+
+
+# API for increasing the number of users
+# It means that the number of vacant rooms will be decreased.
+@api.route('/Down/<string:url>')
+class Down(Resource):
+    def get(self,url):
+        targetKey = ndb.Key(urlsafe=url)
+        target = targetKey.get()
+        
+        NumOfVacantRooms = target.NumOfVacantRooms
+        Before = NumOfVacantRooms
+
+        if NumOfVacantRooms < target.NumOfRooms :
+            NumOfVacantRooms = NumOfVacantRooms + 1
+
+        target.NumOfVacantRooms = NumOfVacantRooms
+        target.put()
+        After = NumOfVacantRooms
+
+        return {'status' : 'ok', 'before':Before, 'after' : After}
 
 
 class Restroom(ndb.Model):
     name = ndb.StringProperty()
     NumOfRooms = ndb.IntegerProperty()
     NumOfVacantRooms = ndb.IntegerProperty()
+
+    @classmethod
+    def get_list(cls):
+        return cls.query()
 
 
 
